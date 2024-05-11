ff8a65dd595fbeee61e80f312c3a29d96b96fc0f
luuktimmermans <lwamtimmermans@gmail.com>
2024-05-11 22:01:58 +0200

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [tailwind()],
+  output: "server",
+  integrations: [tailwind()],
+  adapter: vercel()
 });
