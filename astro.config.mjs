82ac79578ddb539d8727d606201ddb515b78d37e
luuktimmermans <lwamtimmermans@gmail.com>
2024-05-12 01:39:54 +0200

    added ajax and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import htmx from "astro-htmx";
 import { defineConfig } from "astro/config";
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [tailwind(), db()],
+	output: "server",
+	integrations: [tailwind(), db(), [htmx()]],
 	adapter: vercel({
-    webAnalytics: { enabled: true }
-  })
+		webAnalytics: { enabled: true },
+	}),
 });
