b68654f71214b6b7aaf2306fe514981413c3328f
luuktimmermans <lwamtimmermans@gmail.com>
2024-05-12 02:07:41 +0200

    added local storage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import htmx from "astro-htmx";
 import { defineConfig } from "astro/config";
 
 import db from "@astrojs/db";
@@ -8,7 +7,7 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	integrations: [tailwind(), db(), [htmx()]],
+	integrations: [tailwind(), db()],
 	adapter: vercel({
 		webAnalytics: { enabled: true },
 	}),
